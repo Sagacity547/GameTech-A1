5ccc91d0c4bb7cdb1da1c6dd7d82dd803f1008ac
iota547 <juliand547@utexas.edu>
2020-11-06 00:10:06 -0600

    Testing RunAway Method
    testing runAway method, might change to be more just of an idle and check if Player is within a certain range of AI then run away until out of range

--- Assignment2/Assignment2/Project2/Dummy.gd
+++ Assignment2/Assignment2/Project2/Dummy.gd
@@ -17,60 +17,42 @@ var time = 0;
 
 
 #AI specific code
-var AI_STATE : float = 3.0
-const AI_IDLE : float = 1.0
-const AI_ATTACK : float = 2.0
-const AI_RUN : float = 3.0
 var canMove_AI : bool = true
-onready var playerpos = get_node("../Player")
 var timer : float = 3.0
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var aipos= get_node("../AI")
 
-#code to start physics process for game
 func _physics_process(delta):
-	#target = $"../../Pillar"
-	if(AI_STATE == AI_IDLE):
-		move_AI(delta)
-	elif(AI_STATE == AI_ATTACK):
-		fightPlayer(delta)
-	elif(AI_STATE == AI_RUN):
-		runAway(delta)
+	fightPlayer(delta)
 
-#main code that moves AI in an idle state
 func move_AI(delta):
 	timer += delta
 	set_can_walk()
 	if timer >= 2.0: #change directions every 2 seconds
 		timer = 0.0
 		direction = Vector3(random(), 0, random())
-		while(test_move(self.transform, direction)): #make AI run into walls less frequently
-			direction = Vector3(random(), 0, random())
 		direction = direction.normalized()
 		velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
 	handle_gravity()
 	velocity.y = fall_velocity
 	move_and_slide(velocity, Vector3.UP)
-	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func fightPlayer(delta):
+	direction = aipos.transform.origin - self.transform.origin
+	direction = direction.normalized()
+	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
+	handle_gravity()
+	velocity.y = fall_velocity
+	move_and_slide(velocity, Vector3.UP)
 
 func random():
 	randomize()
 	return randi()%361-180 #selects a value between 180 through -180 which is all directions for xy axis
-
-
-func fightPlayer(delta):
-	direction = playerpos.transform.origin - self.transform.origin
-	direction = direction.normalized()
-	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
-	handle_gravity()
-	velocity.y = fall_velocity
-	move_and_slide(velocity, Vector3.UP)
-
-func runAway(delta):
-	direction = self.transform.origin - playerpos.transform.origin
-	direction = direction.normalized()
-	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta)
-	handle_gravity()
-	velocity.y = fall_velocity
-	move_and_slide(velocity, Vector3.UP)
 
 #applies gravity to player and handles flying and jumping
 func handle_gravity():
@@ -97,4 +79,3 @@ func set_can_walk():
 			canWalk = false
 		else: 
 			canWalk = true
-
